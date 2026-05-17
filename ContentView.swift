08ae6961c1afe194cb3c38919f07e5970eddb1be
gate2next-creator <gate2next@gmail.com>
2026-05-17 23:48:04 +0200

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Market_Hub
+//
+//  Created by omer faruk on 01.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Market_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Market_HubApp.swift
+//  Market_Hub
+//
+//  Created by omer faruk on 01.05.26.
+//
+
+import SwiftUI
+
+@main
+struct Market_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
